3554c315cabd817d376bf0e7c427f240792c4d07
kei-g <km.8k6ce+github@gmail.com>
2022-10-08 20:09:44 +0900

    :whale: Support ubuntu also as a base image
    - focal and jammy are supported
    
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ CONTAINERS=$(docker ps --all --filter=status=exited --format="{{.ID}}" | xargs)
 IMAGES=$(docker images --filter=dangling=true --format="{{.ID}}" | xargs)
 [ -z "$IMAGES" ] || docker rmi "$IMAGES"
 
-docker build -t snowstep/llvm .
+for name in $(ls linux); do
+  docker build -f linux/$name/Dockerfile -t snowstep/llvm:$name .
+done
