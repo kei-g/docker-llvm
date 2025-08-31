12d21bfa9f570767fd46b52020eb2cc9f83116e2
kei-g <km.8k6ce+github@gmail.com>
2025-08-31 19:32:44 +0900

    :whale: Use llvm-19 to build llvm-22
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- symlink.sh
+++ symlink.sh
@@ -2,17 +2,17 @@
 set -e
 find /usr/bin \
 		\( \
-			-name clang\*-${MAJOR_VERSION:-22} \
-			-o -name flang\*-${MAJOR_VERSION:-22} \
-			-o -name lld\*-${MAJOR_VERSION:-22} \
-			-o -name llvm\*-${MAJOR_VERSION:-22} \
+			-name clang\*-${MAJOR_VERSION:-19} \
+			-o -name flang\*-${MAJOR_VERSION:-19} \
+			-o -name lld\*-${MAJOR_VERSION:-19} \
+			-o -name llvm\*-${MAJOR_VERSION:-19} \
 		\) \
 	-exec bash -c '\
 i=1; \
 while [[ $i -lt $# ]]; do \
 	eval name=\${$i##*/}; \
 	eval name=\${name%\-*}; \
-	ln -fs $name-${MAJOR_VERSION:-22} /usr/bin/$name; \
+	ln -fs $name-${MAJOR_VERSION:-19} /usr/bin/$name; \
 	i=$((i + 1)); \
 done \
 ' -- {} +
